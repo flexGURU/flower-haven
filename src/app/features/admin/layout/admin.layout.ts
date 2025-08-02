6de8f073b58791b5c4ca72a332d4b7eb2fc45fba
flexGURU <mukunajohn329@gmail.com>
2025-08-02 14:49:03 +0300

    feat(admin): add navigation component to admin layout

--- src/app/features/admin/layout/admin.layout.ts
+++ src/app/features/admin/layout/admin.layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AdminHeaderComponent } from "./header/admin_header";
+import { NavComponent } from "./nav/nav";
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin.layout.html',
-  imports: [RouterOutlet, AdminHeaderComponent],
+  imports: [RouterOutlet, AdminHeaderComponent, NavComponent],
 })
 export class AdminLayout {}
---
+++ src/app/features/admin/layout/nav/nav.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.html',
+  imports: [MenubarModule],
+})
+export class NavComponent {
+  items!: any;
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Dashboard',
+        icon: 'pi pi-home',
+        routerLink: 'dashboard',
+      },
+      {
+        label: 'Category',
+        icon: 'pi pi-folder',
+        routerLink: 'category',
+      },
+
+      {
+        label: 'Product list',
+        icon: 'pi pi-tags',
+        routerLink: 'catalog',
+      },
+    ];
+  }
+}
