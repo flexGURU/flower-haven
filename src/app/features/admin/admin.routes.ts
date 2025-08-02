28a4cd8f9292e22f59cecb9e2301229023ddc043
flexGURU <mukunajohn329@gmail.com>
2025-08-02 14:40:15 +0300

    feat(admin): implement admin layout with dashboard and header components

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -1,5 +1,16 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
+import { AdminLayout } from './layout/admin.layout';
+import { DashboardComponent } from './components/dashboard/dashboard';
 
 export const adminRoutes: Routes = [
-    {}
-]
+  {
+    path: '',
+    component: AdminLayout,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+    ],
+  },
+];
--- src/app/features/admin/layout/admin.layout.ts
+++ src/app/features/admin/layout/admin.layout.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AdminHeaderComponent } from "./header/admin_header";
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin.layout.html',
+  imports: [RouterOutlet, AdminHeaderComponent],
 })
 export class AdminLayout {}
