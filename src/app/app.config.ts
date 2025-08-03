c21cb823e32f1ffec5c2582eb2cd86fb632e6189
flexGURU <mukunajohn329@gmail.com>
2025-08-03 12:50:27 +0300

    feat: integrate Firebase with Angular application
    - Added @angular/fire dependency to package.json.
    - Updated app.config.ts to initialize Firebase with AngularFire.
    - Created firebase.ts service for future Firebase interactions.
    - Added environment configuration files for development and production with Firebase credentials.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { routes } from './app.routes';
 import PurplePreset from './preset';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { firebaseConfig } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom([
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFirestoreModule,
+    ]),
     provideHttpClient(withFetch()),
     provideAnimationsAsync(),
     provideZoneChangeDetection({ eventCoalescing: true }),
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAbMydCzWBRMCc0S9ZHD-6-O4IbM-L7EkI',
+  authDomain: 'floral-haven-97e6a.firebaseapp.com',
+  projectId: 'floral-haven-97e6a',
+  storageBucket: 'floral-haven-97e6a.firebasestorage.app',
+  messagingSenderId: '252662323921',
+  appId: '1:252662323921:web:21529706a2ea1d21a383f0',
+  measurementId: 'G-9S6749E9Z6',
+};
