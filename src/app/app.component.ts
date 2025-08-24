5aa2d7fa01c8a5a8ef3207a4b2169ddfd8cf1020
flexGURU <mukunajohn329@gmail.com>
2025-08-25 00:03:34 +0300

    fix: remove provider service error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { Toast } from "primeng/toast";
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule, RouterOutlet],
+  imports: [ButtonModule, RouterOutlet, Toast],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [MessageService]
 })
 export class AppComponent {
   title = 'flower-haven';
