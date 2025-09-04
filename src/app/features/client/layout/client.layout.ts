93e0295af5fa77373e5c6d2a4ba74bba7aa5eed4
flexGURU <mukunajohn329@gmail.com>
2025-09-04 19:43:33 +0300

    feat: implement dynamic header visibility on scroll in client layout

--- src/app/features/client/layout/client.layout.ts
+++ src/app/features/client/layout/client.layout.ts
@@ -1,10 +1,36 @@
-import { Component } from "@angular/core";
-import { HeaderComponent } from "./header/header";
-import { RouterOutlet } from "@angular/router";
-import { FoooterComponent } from "./footer/footer";
+import { Component, HostListener, signal } from '@angular/core';
+import { HeaderComponent } from './header/header';
+import { RouterOutlet } from '@angular/router';
+import { FoooterComponent } from './footer/footer';
 @Component({
-  selector: "app-client-layout",
-  templateUrl: "./client.layout.html",
+  selector: 'app-client-layout',
+  templateUrl: './client.layout.html',
   imports: [HeaderComponent, RouterOutlet, FoooterComponent],
 })
-export class ClientLayout {}
+export class ClientLayout {
+  isHeaderVisible = signal(true);
+  private lastScrollTop = 0;
+  private ticking = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (!this.ticking) {
+      window.requestAnimationFrame(() => {
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+        if (scrollTop > this.lastScrollTop + 10) {
+          // scrolling down
+          this.isHeaderVisible.set(false);
+        } else if (scrollTop < this.lastScrollTop - 10) {
+          // scrolling up
+          this.isHeaderVisible.set(true);
+        }
+
+        this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+        this.ticking = false;
+      });
+
+      this.ticking = true;
+    }
+  }
+}
